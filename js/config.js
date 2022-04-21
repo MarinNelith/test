f7b7a9fe83908bad81eda15a6b49c83e4242d6ca
Marin-WorkPC <marin.pignarre@orange.fr>
2022-04-21 09:47:38 +0200

    initial commit

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-1_tIRjCR5gj', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4os8smbvb5nllj7b0rqkrr7h8v', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
